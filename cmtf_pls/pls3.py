0596e5a44f883f2b358e9d81ac5742d34a45a551
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2022-07-17 15:26:53 -0700

    Added predict method to 3-mode tensor PLS

--- cmtf_pls/pls3.py
+++ cmtf_pls/pls3.py
@@ -1,10 +1,25 @@
+import numpy as np
+import tensorly as tl
+from tensorly.tenalg import khatri_rao
+
 from ._pls_tensor import *
+
 
 class ThreeModePLS(PLSTensor):
     def __init__(self, *args):
         super().__init__(*args)
 
-    def fit(self):
+    def _init_factors(self, x, y):
+        assert x.shape[0] == y.shape[0]
+        assert y.ndim <= 2
+        self.Xdim = x.ndim
+        self.X = x
+        self.Y = y
+        self.Xfacs = [np.zeros((l, self.num_comp)) for l in x.shape]
+        self.Yfacs = [np.zeros((l, self.num_comp)) for l in y.shape]
+
+    def fit(self, x, y):
+        self._init_factors(x, y)
         self.preprocess()
         X, Y = self.X.copy(), self.Y.copy()
         assert X.ndim == 3
@@ -22,4 +37,15 @@ class ThreeModePLS(PLSTensor):
             X = X - CPTensor((None, self.Xfacs)).to_tensor()
             Y = Y - self.Xfacs[0] @ pinv(self.Xfacs[0]) @ Y
 
-        self.Yfacs = [self.Xfacs[0] @ pinv(self.Xfacs[0]) @ Y]
+        Y_loading, _, _, _ = np.linalg.lstsq(self.Xfacs[0], self.Y, rcond=-1)
+        self.Yfacs = [
+            self.Xfacs[0] @ pinv(self.Xfacs[0]) @ self.Y,
+            Y_loading
+        ]
+
+    def predict(self, to_predict):
+        factors_kr = khatri_rao(self.Xfacs, skip_matrix=0)
+        unfolded = tl.unfold(to_predict, 0)
+        scores, _, _, _ = np.linalg.lstsq(factors_kr, unfolded.T, rcond=-1)
+
+        return scores.T @ self.Yfacs[1]
