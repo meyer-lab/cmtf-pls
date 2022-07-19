d7049f3321b538013a1b5a5b0bf946030cccbc99
Cyrillus <cyztan@gmail.com>
2022-07-19 15:01:37 -0700

    Restore figure 1 plot

--- cmtf_pls/pls3.py
+++ cmtf_pls/pls3.py
@@ -9,7 +9,31 @@ class ThreeModePLS(PLSTensor):
     def __init__(self, *args):
         super().__init__(*args)
 
-    def fit(self, X, Y, tol=1e-10, max_iter=100):
+    def fit(self, X, Y):
+        X, Y = self.preprocess(X, Y)
+        Y = Y.flatten()
+        assert X.ndim == 3
+        assert Y.ndim == 1
+        self.X_factors = [np.zeros((l, self.n_components)) for l in X.shape]
+        assert X.shape[0] == Y.shape[0]
+        for a in range(self.n_components):
+            svd_res = np.linalg.svd(mode_dot(X, Y, 0))
+            self.X_factors[1][:, a] = svd_res[0][:, 0]
+            self.X_factors[2][:, a] = svd_res[2].T[:, 0]
+            Tt = X.copy()
+            Tt = mode_dot(Tt, self.X_factors[1][:, a], 1)
+            Tt = mode_dot(Tt, self.X_factors[2][:, a], 1)
+            self.X_factors[0][:, a] = Tt
+            X = X - CPTensor((None, self.X_factors)).to_tensor()
+            Y = Y - self.X_factors[0] @ pinv(self.X_factors[0]) @ Y
+
+        Y_loading, _, _, _ = np.linalg.lstsq(self.X_factors[0], self.original_Y - self.Y_mean, rcond=-1)
+        self.Y_factors = [
+            self.X_factors[0] @ pinv(self.X_factors[0]) @ (self.original_Y - self.Y_mean),
+            Y_loading
+        ]
+
+    def fit_v2(self, X, Y, tol=1e-10, max_iter=100):
         X, Y = self.preprocess(X, Y)
         assert X.ndim == 3
         if Y.ndim == 1:
