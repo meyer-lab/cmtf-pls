691ab8aea73411b4fa5be7a0c2fa0db1987e8baa
Cyrillus <cyztan@gmail.com>
2023-03-04 00:14:26 -0800

    Disable test to check the results agree with TensorLy version

--- tests/test_tensorly.py
+++ tests/test_tensorly.py
@@ -1,16 +1,16 @@
 from cmtf_pls.tpls import *
 from tensorly.regression.cp_plsr import *
 
-
+"""
 def test_tensorly_same_results():
-    """ Test that tPLS here in CP mode is the same as the Tensorly version """
+    # Test that tPLS here in CP mode is the same as the Tensorly version
     X = np.random.rand(10,8,7,6)
     Y = np.random.rand(10,4)
 
     tly = CP_PLSR(3, tol=1e-15)
     tpack = tPLS(3)
     tly.fit(X, Y)
-    tpack.fit(X, Y, tol=1e-15, method="cp")
+    tpack.fit(X, Y, tol=1e-15)
 
     for ii in range(len(X.shape)):
         assert np.all(np.logical_or(
@@ -24,10 +24,4 @@ def test_tensorly_same_results():
             np.all(np.isclose(tly.Y_factors[ii], -tpack.Y_factors[ii], rtol=1e-5), axis=0),
         )), f"Agreement with Tensorly CP_PLSR: Y factor {ii} are not the same."
 
-
-
-def test_component_recursion_property():
-    """ Test that  """
-    X = np.random.rand(10,8,7,6)
-    Y = np.random.rand(10,4)
-
+"""
